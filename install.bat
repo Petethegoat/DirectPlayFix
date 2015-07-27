8a1e69d09d665029ade8b877fe3d40c8e75487a4
Pete Goodfellow <petethegoat@gmail.com>
2015-07-27 19:58:00 +0100

    Updated install.bat to handle directory paths with spaces correctly.

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @ECHO off
-%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\regasm.exe %~dp0DVoiceEmul.dll /codebase
+%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\regasm.exe %~sdp0DVoiceEmul.dll /codebase
 echo.
 echo Done!
 echo.
